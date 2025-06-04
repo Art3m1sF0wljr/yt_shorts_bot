ec3d6b5ca09bad641923c408e290d8e3d899eeb8
dcazacu <158522530+Art3m1sF0wljr@users.noreply.github.com>
2025-06-04 20:23:56 +0200

    Update youtubeupload.py

--- youtubeupload.py
+++ youtubeupload.py
@@ -7,6 +7,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request  # Added this import
 
 # Configuration
 VIDEOS_FOLDER = 'videos'
